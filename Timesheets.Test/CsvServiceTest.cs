4dd7c608ef65cf76f8a42009f9ba06e99840e1c0
Alan Broadhead <alan.broadhead@gmail.com>
2024-09-21 15:56:24 +0100

    Update CsvServiceTest.cs

--- Timesheets.Test/CsvServiceTest.cs
+++ Timesheets.Test/CsvServiceTest.cs
@@ -10,8 +10,20 @@ public class CsvServiceTest
     {
         var csvService = new CsvService();
         var timesheets = new List<Timesheet>()
-        { new Timesheet{ TimesheetEntry = new TimesheetEntry
-        { Date ="01/01/2001", FirstName="first", Hours = "8", LastName ="last", Project ="proj" }, TotalHours = "8" } };
+        { 
+            new Timesheet
+            { 
+                TimesheetEntry = new TimesheetEntry
+                   { 
+                        Date ="01/01/2001", 
+                        FirstName="first",
+                        Hours = "8",
+                        LastName ="last",
+                        Project ="proj" 
+                    },
+                TotalHours = "8" 
+            } 
+        };
         var csv = csvService.CreateCSV(timesheets);
         Assert.Equal("Id, FirstName, LastName, Date, Project, Hours, TotalHours\r\n0, first, last, 01/01/2001, proj, 8, 8\r\n", csv);
     }
